5e11f90c386aed6e68f6cbfa50c292f3f95cc3c9
demostanis <demostanis@protonmail.com>
2024-06-23 00:27:17 +0200

    correct function signatures

--- libasm.h
+++ libasm.h
@@ -1,8 +1,8 @@
 #pragma once
 
-int		ft_strlen(char *s);
-char	*ft_strcpy(char *s1, char *s2);
-int		ft_strcmp(char *s1, char *s2);
-int		ft_write(int fd, char *buf, size_t count);
-int		ft_read(int fd, char *buf, size_t count);
-char	*ft_strdup(char *s);
+size_t	ft_strlen(const char *s);
+char	*ft_strcpy(char *s1, const char *s2);
+int		ft_strcmp(const char *s1, const char *s2);
+ssize_t	ft_write(int fd, const void *buf, size_t count);
+int		ft_read(int fd, void *buf, size_t count);
+char	*ft_strdup(const char *s);
